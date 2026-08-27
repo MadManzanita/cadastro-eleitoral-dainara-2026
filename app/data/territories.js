c5888ff80e5fda128fa99b1b6b5d579f67150cbc
MadManzanita <taeyeonsistans@gmail.com>
2026-08-26 22:08:14 -0400

    Atualiza zonas e bairros de Manaus conforme planilha fornecida

--- app/data/territories.js
+++ app/data/territories.js
@@ -3,19 +3,108 @@ export const AMAZONAS_MUNICIPALITIES = [
 ];
 
 export const MANAUS_ZONES = {
-  "Adrianópolis":"Centro-Sul","Aleixo":"Centro-Sul","Chapada":"Centro-Sul","Flores":"Centro-Sul","Nossa Senhora das Graças":"Centro-Sul","Parque 10 de Novembro":"Centro-Sul",
-  "Alvorada":"Centro-Oeste","Bairro da Paz":"Oeste","Compensa":"Oeste","Da Paz":"Oeste","Dom Pedro I":"Oeste","Glória":"Oeste","Lírio do Vale":"Oeste","Nova Esperança":"Oeste","Planalto":"Oeste","Ponta Negra":"Oeste","Redenção":"Oeste","Santo Agostinho":"Oeste","Santo Antônio":"Oeste","São Jorge":"Oeste","São Raimundo":"Oeste","Tarumã":"Oeste","Tarumã-Açu":"Oeste","Vila da Prata":"Oeste",
-  "Armando Mendes":"Leste","Colônia Antônio Aleixo":"Leste","Coroado":"Leste","Distrito Industrial II":"Leste","Gilberto Mestrinho":"Leste","Jorge Teixeira":"Leste","Mauazinho":"Leste","Puraquequara":"Leste","São José Operário":"Leste","Tancredo Neves":"Leste","Zumbi dos Palmares":"Leste",
-  "Cidade de Deus":"Norte","Cidade Nova":"Norte","Colônia Santo Antônio":"Norte","Colônia Terra Nova":"Norte","Lago Azul":"Norte","Monte das Oliveiras":"Norte","Nova Cidade":"Norte","Novo Aleixo":"Norte","Novo Israel":"Norte","Santa Etelvina":"Norte",
-  "Betânia":"Sul","Cachoeirinha":"Sul","Centro":"Sul","Colônia Oliveira Machado":"Sul","Crespo":"Sul","Distrito Industrial I":"Sul","Educandos":"Sul","Japiim":"Sul","Morro da Liberdade":"Sul","Nossa Senhora Aparecida":"Sul","Petrópolis":"Sul","Praça 14 de Janeiro":"Sul","Presidente Vargas":"Sul","Raiz":"Sul","Santa Luzia":"Sul","São Francisco":"Sul","São Geraldo":"Sul","São Lázaro":"Sul","Vila Buriti":"Sul"
+  // Zona Centro-Oeste
+  "Alvorada": "Centro-Oeste", "Bairro da Paz": "Centro-Oeste", "Dom Pedro": "Centro-Oeste", "Planalto": "Centro-Oeste",
+  "Redenção": "Centro-Oeste", "Ajuricaba": "Centro-Oeste", "Aripuanã": "Centro-Oeste", "Aristocratico": "Centro-Oeste",
+  "Belvedere": "Centro-Oeste", "Campos Eliseos": "Centro-Oeste", "Canaã": "Centro-Oeste", "Com Ouro Verde": "Centro-Oeste",
+  "Déborah": "Centro-Oeste", "Flamanal": "Centro-Oeste", "Hileia": "Centro-Oeste", "Jardim Versailles": "Centro-Oeste",
+  "Jurua": "Centro-Oeste", "Jussara": "Centro-Oeste", "Kissia": "Centro-Oeste", "Marina Taua": "Centro-Oeste",
+  "Parque e Gomes": "Centro-Oeste", "Promorar": "Centro-Oeste", "Rio Jamar": "Centro-Oeste", "Santa Barbara": "Centro-Oeste",
+  "Santa Teresinha": "Centro-Oeste", "Santos Dumont": "Centro-Oeste", "Vista Bela": "Centro-Oeste",
+
+  // Zona Norte
+  "Cidade de Deus": "Norte", "Cidade Nova": "Norte", "Colonia Santo Antonio": "Norte", "Colonia Terra Nova": "Norte",
+  "Lago Azul": "Norte", "Monte das Oliveiras": "Norte", "Nova Cidade": "Norte", "Novo Aleixo": "Norte",
+  "Novo Israel": "Norte", "Santa Etelvina": "Norte", "Agnus Day": "Norte", "aguas Claras": "Norte",
+  "Alfredo Nascimento": "Norte", "Aliança com Deus": "Norte", "Amadeu Botelho": "Norte", "Amazonino Mendes": "Norte",
+  "América do Sul": "Norte", "Américo Medeiros": "Norte", "Bairro do Céu": "Norte", "Bairro Novo": "Norte",
+  "Boas Novas": "Norte", "Campo Dourado": "Norte", "Carlos Braga": "Norte", "Colonia Cachoeira Grande": "Norte",
+  "Colonia Japonesa": "Norte", "Comagi": "Norte", "Fazendinha": "Norte", "Florestal": "Norte",
+  "Francisca Mendes": "Norte", "Galileia": "Norte", "Gustavo Nascimento": "Norte", "Ismail Aziz": "Norte",
+  "Jardim Canaranas": "Norte", "Jardim Fortaleza": "Norte", "Jardim Independente": "Norte", "José Bonifacio": "Norte",
+  "Luiz Otavio": "Norte", "Manoa": "Norte", "Monte Pascoal": "Norte", "Monte Sinai": "Norte",
+  "Mundo Novo": "Norte", "Mutirão": "Norte", "Nossa Senhora de Fatima": "Norte", "Nossa Senhora do Perpétuo Socorro": "Norte",
+  "Novo Millenium": "Norte", "Núcleo 1": "Norte", "Núcleo 2": "Norte", "Núcleo 3": "Norte",
+  "Núcleo 4": "Norte", "Núcleo 5": "Norte", "Núcleo 6": "Norte", "Núcleo 7": "Norte",
+  "Núcleo 8": "Norte", "Núcleo 9": "Norte", "Núcleo 10": "Norte", "Núcleo 11": "Norte",
+  "Núcleo 12": "Norte", "Núcleo 13": "Norte", "Núcleo 14": "Norte", "Núcleo 15": "Norte",
+  "Núcleo 16": "Norte", "Núcleo 17": "Norte", "Núcleo 18": "Norte", "Núcleo 19": "Norte",
+  "Núcleo 20": "Norte", "Núcleo 21": "Norte", "Núcleo 22": "Norte", "Núcleo 23": "Norte",
+  "Núcleo 24": "Norte", "Omar Aziz": "Norte", "Oswaldo Américo": "Norte", "Oswaldo Frota": "Norte",
+  "Parque Canaã": "Norte", "Parque Celebridade": "Norte", "Parque das Garças": "Norte", "Parque das Nações": "Norte",
+  "Parque dos Buritis": "Norte", "Parque dos Ingleses": "Norte", "Parque Senador Jefferson Péres": "Norte", "Raio de Sol": "Norte",
+  "Renato Souza Pinto": "Norte", "Riacho Doce": "Norte", "Ribeiro Júnior": "Norte", "Rio Piorini": "Norte",
+  "Santa Marta": "Norte", "São João": "Norte", "Vale do Sinai": "Norte", "Vila Manaus": "Norte",
+  "Vila Nova": "Norte", "Vitoria Régia": "Norte",
+
+  // Zona Leste
+  "Armando Mendes": "Leste", "Colonia Antonio Aleixo": "Leste", "Coroado": "Leste", "Distrito Industrial II": "Leste",
+  "Gilberto Mestrinho": "Leste", "Jorge Teixeira": "Leste", "Mauazinho": "Leste", "Puraquequara": "Leste",
+  "São José Operario": "Leste", "Tancredo Neves": "Leste", "Zumbi dos Palmares": "Leste", "Acariquara": "Leste",
+  "Asteca": "Leste", "Bela Vista": "Leste", "Braga Mendes": "Leste", "Buritizal": "Leste",
+  "Castanheira": "Leste", "Cidade do Leste": "Leste", "Colina do Aleixo": "Leste", "Com Colonia Antonio Aleixo": "Leste",
+  "Com Nova Esperança": "Leste", "Com Planalto": "Leste", "Do Vale": "Leste", "Fé": "Leste",
+  "Grande Vitoria": "Leste", "Itacolomi": "Leste", "João Bosco": "Leste", "João Paulo": "Leste",
+  "Monte Sião": "Leste", "Nova Conquista": "Leste", "Nova Floresta": "Leste", "Nova Jerusalém": "Leste",
+  "Nova Vitoria": "Leste", "Novo Reino": "Leste", "Ouro Verde": "Leste", "Parque Rouxinol": "Leste",
+  "Parque São Cristovão": "Leste", "Parque Sucupiras": "Leste", "Petro": "Leste", "Portelinha": "Leste",
+  "Presidente Lula": "Leste", "Rio Negro": "Leste", "Santa Inês": "Leste", "São Lucas": "Leste",
+  "Sharp": "Leste", "Tiradentes": "Leste", "Valparaiso": "Leste", "11 de Maio": "Leste",
+
+  // Zona Sul
+  "Betânia": "Sul", "Cachoeirinha": "Sul", "Centro": "Sul", "Colonia Oliveira Machado": "Sul",
+  "Crespo": "Sul", "Distrito Industrial I": "Sul", "Educandos": "Sul", "Japiim": "Sul",
+  "Morro da Liberdade": "Sul", "Nossa Senhora de Aparecida": "Sul", "Petropolis": "Sul", "Praça 14 de Janeiro": "Sul",
+  "Presidente Vargas": "Sul", "Raiz": "Sul", "Santa Luzia": "Sul", "São Francisco": "Sul",
+  "São Lazaro": "Sul", "Vila Buriti": "Sul", "Andreaza": "Sul", "Baixa da Égua": "Sul",
+  "Ceasa": "Sul", "Costa e Silva": "Sul", "Getúlio Vargas": "Sul", "Jap. Codajas": "Sul",
+  "Japiinlândia": "Sul", "Jardim Brasil": "Sul", "Jardim Petropolis": "Sul", "Jarulândia": "Sul",
+  "Jerusalém": "Sul", "Lagoa Verde": "Sul", "Matinha": "Sul", "Nova República": "Sul",
+  "Quarenta": "Sul", "São Sebastião": "Sul", "Vale do Sol": "Sul", "Vila Humaita": "Sul",
+  "31 de Março": "Sul", "22 de Outubro": "Sul",
+
+  // Zona Centro-Sul
+  "Adrianopolis": "Centro-Sul", "Aleixo": "Centro-Sul", "Chapada": "Centro-Sul", "Flores": "Centro-Sul",
+  "Nossa Senhora das Graças": "Centro-Sul", "Parque 10 de Novembro": "Centro-Sul", "São Geraldo": "Centro-Sul", "Abilio Nery": "Centro-Sul",
+  "Aefam": "Centro-Sul", "Agricentro": "Centro-Sul", "Arthur Reis": "Centro-Sul", "Anavilhanas": "Centro-Sul",
+  "Bairro da União": "Centro-Sul", "Barra Bela": "Centro-Sul", "Beija Flor": "Centro-Sul", "Beverly Hills": "Centro-Sul",
+  "Castelo Branco": "Centro-Sul", "Celetra": "Centro-Sul", "Copacabana": "Centro-Sul", "Duque de Caxias": "Centro-Sul",
+  "Eldorado": "Centro-Sul", "Encol": "Centro-Sul", "Fernando Fritz": "Centro-Sul", "Floral": "Centro-Sul",
+  "Huascar Angelim": "Centro-Sul", "Ica Magistral": "Centro-Sul", "Ica Paraiba": "Centro-Sul", "Ipanema": "Centro-Sul",
+  "Jardim Amazonas": "Centro-Sul", "Jardim California": "Centro-Sul", "Jardim Espanha": "Centro-Sul", "Jardim Eucaliptos": "Centro-Sul",
+  "Jardim Haydea": "Centro-Sul", "Jardim Iara": "Centro-Sul", "Jardim Imperial": "Centro-Sul", "Jardim Italia": "Centro-Sul",
+  "Jardim Meridional": "Centro-Sul", "Jardim Olivia": "Centro-Sul", "Jardim Oriente": "Centro-Sul", "Jardim Paulista": "Centro-Sul",
+  "Jardim Primavera": "Centro-Sul", "Jardim Sakura": "Centro-Sul", "Jardim Yolanda": "Centro-Sul", "Juliana": "Centro-Sul",
+  "Levillage Blanc": "Centro-Sul", "Malibu": "Centro-Sul", "Morada do Sol": "Centro-Sul", "Mucuripe": "Centro-Sul",
+  "Murici": "Centro-Sul", "Nascente aguas Claras": "Centro-Sul", "Nova Friburgo": "Centro-Sul", "Novo Horizonte": "Centro-Sul",
+  "Novo Mundo": "Centro-Sul", "Parque das Laranjeiras": "Centro-Sul", "Parque Residencial Monte Libano": "Centro-Sul", "Parque Shangri-la": "Centro-Sul",
+  "Parque Tropical": "Centro-Sul", "Pindorama": "Centro-Sul", "Portal do Japão": "Centro-Sul", "Presidente Getúlio Vargas": "Centro-Sul",
+  "Real": "Centro-Sul", "Rio Mar": "Centro-Sul", "Rio Maracanã": "Centro-Sul", "Samambaia": "Centro-Sul",
+  "Santa Cruz": "Centro-Sul", "São José do Rio Negro": "Centro-Sul", "Sausalito": "Centro-Sul", "Sol Morar": "Centro-Sul",
+  "Tapajos": "Centro-Sul", "Uirapuru": "Centro-Sul", "Verdes Mares": "Centro-Sul", "Vieiralves": "Centro-Sul",
+  "Vila Amazonas": "Centro-Sul", "Vila da Barra": "Centro-Sul", "Vila do Rey": "Centro-Sul", "Vila Mariana": "Centro-Sul",
+  "Vila Municipal": "Centro-Sul",
+
+  // Zona Oeste
+  "Compensa": "Oeste", "Gloria": "Oeste", "Lirio do Vale": "Oeste", "Nova Esperança": "Oeste",
+  "Ponta Negra": "Oeste", "Santo Agostinho": "Oeste", "Santo Antonio": "Oeste", "São Jorge": "Oeste",
+  "São Raimundo": "Oeste", "Tarumã": "Oeste", "Tarumã-Açu": "Oeste", "Vila da Prata": "Oeste",
+  "Augusto Montenegro": "Oeste", "Ayapua": "Oeste", "Campos Sales": "Oeste", "Com Promorar": "Oeste",
+  "Com Vitoria Régia": "Oeste", "Ipase": "Oeste", "Jardim América": "Oeste", "Jardim dos Barés": "Oeste",
+  "Jardim Europa": "Oeste", "Jesus Me Deu": "Oeste", "Mediterrâneo": "Oeste", "Meu Bem, Meu Mal": "Oeste",
+  "Parque Aruanã": "Oeste", "Parque do Lago": "Oeste", "Parque Residencial Itapuranga": "Oeste", "Parque Riachuelo": "Oeste",
+  "Rio Xingu": "Oeste", "Rumo Certo": "Oeste", "Santo Antonio Areal": "Oeste", "Santo Antonio Igreja": "Oeste",
+  "Santo Antonio Manda Brasa": "Oeste", "São Pedro": "Oeste", "União da Vitoria": "Oeste", "Vila Marinho": "Oeste",
+  "Vila Verde": "Oeste", "Vivenda Verde": "Oeste"
 };
 
 export const MANAUS_NEIGHBORHOODS = Object.keys(MANAUS_ZONES).sort((a,b) => a.localeCompare(b, "pt-BR"));
 
-// O IBGE registra 469 bairros no Amazonas no Censo 2022. O próprio IBGE ressalta
-// que nem todo município possui bairros legalmente instituídos. Por isso o campo
-// do sistema é "Bairro / Localidade". Onde ainda não temos uma lista municipal
-// validada, não inventamos nomes: usamos apenas opções neutras de localização.
+/**
+ * Fonte: planilha "ZONAS DE MANAUS.xlsx" fornecida para o projeto.
+ * A lista abaixo contempla os bairros e sub-bairros/localidades presentes
+ * na planilha, associados à respectiva zona de Manaus.
+ */
 export const AMAZONAS_TERRITORIES = Object.fromEntries(
   AMAZONAS_MUNICIPALITIES.map((municipio) => [
     municipio,
